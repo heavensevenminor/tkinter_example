5c2bb5d757ee9025c09700b42918a2d801faee14
titeles <titeles@naver.com>
2022-11-27 17:29:03 +0900

    rename

--- part_02/window_option.py
+++ part_02/window_option.py
@@ -19,9 +19,10 @@ master.maxsize(screen_width//2,screen_height//2)
 master.attributes('-alpha', 1)
 master.attributes('-topmost', True)
 
+# 같은 폴더에 있는 아이콘 불러 오기
 master.iconbitmap(default=Path(__file__).with_name('favicon.ico'))
 
-# 작업표시줄에 아이콘도 변경
+# 작업표시줄에 아이콘 변경
 try:
   from ctypes import windll  # Only exists on Windows.
   windll.shell32.SetCurrentProcessExplicitAppUserModelID("mycompany.myproduct.subproduct.version")
